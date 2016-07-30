fc6472076328b96f07dc87dd1e1e4ac9830c3b14
pakjkwan <pakjkwan@gmail.com>
2016-07-30 11:29:40 +0900

    2nd

---
+++ app/src/main/java/com/week/app/week160730/member/MemberListActivity.java
@@ -0,0 +1,15 @@
+package com.week.app.week160730.member;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.week.app.week160730.R;
+
+public class MemberListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_list);
+    }
+}
